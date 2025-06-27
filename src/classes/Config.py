e8e36c62251db4b646ce7f6dafe695b949060f05
Akash Kumar Sharma <akashhdr@gmail.com>
2025-06-27 14:08:43 +0530

    Added setConfigValue in Config.py

--- src/classes/Config.py
+++ src/classes/Config.py
@@ -1,27 +1,41 @@
-import json, sqlite3, os, sys, requests, copy
+import json
+import sqlite3
+import os
+import sys
+import requests
+import copy
 
-class Config():
-    '''
+
+class Config:
+    """
     Config class to set configuration files, secret file, initializing database and brocker session.
     \nInstantiation of this class is restricted.
-    '''
-    __configFilePath=os.path.dirname(os.path.abspath(sys.argv[0]))+'/'+"config.json"
-    __secretFilePath=os.path.dirname(os.path.abspath(sys.argv[0]))+'/'+"secret.json"
-    __stockDB = 'stocks.db'
-    __backtestingDB = 'backtestingStocks.db'
+    """
+
+    __configFilePath = (
+        os.path.dirname(os.path.abspath(sys.argv[0])) + "/" + "config.json"
+    )
+    __secretFilePath = (
+        os.path.dirname(os.path.abspath(sys.argv[0])) + "/" + "secret.json"
+    )
+    __stockDB = "stocks.db"
+    __backtestingDB = "backtestingStocks.db"
     __configValues = {}
     __secretValues = {}
     __dbConnections = list()
     __brokerSession = requests.session()
-    
+
     # To restrict object creation, self is removed from parameter
     def __init__() -> None:
         raise Exception("Object cannot be created for Config class")
-    
+
     @staticmethod
-    def setupEnvironment(isBacktesting = False):
-        return (copy.deepcopy(Config.getBrokerSession()), Config.getDBCursors(isBacktesting))
-    
+    def setupEnvironment(isBacktesting=False):
+        return (
+            copy.deepcopy(Config.getBrokerSession()),
+            Config.getDBCursors(isBacktesting),
+        )
+
     @staticmethod
     def setConfigFiles(configFilePath=None, secretFilePath=None):
         if configFilePath is not None:
@@ -29,7 +43,7 @@ class Config():
         if secretFilePath is not None:
             Config.__secretFilePath = secretFilePath
         Config.__loadConfigurations()
-        
+
     @staticmethod
     def getConfigValues(index):
         Config.__loadConfigurations()
@@ -37,37 +51,44 @@ class Config():
             return None
         else:
             return Config.__configValues[index]
-    
+
     @staticmethod
-    def getDBCursors(isBackTesting = False):
+    def setConfigValues(index, value):
+        Config.__configValues[index] = value
+        file = open("config.json", "w")
+        status = file.write(json.dumps(Config.__configValues))
+        file.close()
+        return status
+
+    @staticmethod
+    def getDBCursors(isBackTesting=False):
         Config.__initDBConnections()
         if not isBackTesting:
             return (Config.__dbConnections[0].cursor(), Config.__dbConnections[0])
         else:
             return (Config.__dbConnections[1].cursor(), Config.__dbConnections[1])
-    
+
     @staticmethod
     def __loadConfigurations():
-        config = open(Config.__configFilePath, 'r')
+        config = open(Config.__configFilePath, "r")
         Config.__configValues = json.loads(config.read())
         config.close()
-        secret = open(Config.__secretFilePath, 'r')
+        secret = open(Config.__secretFilePath, "r")
         Config.__secretValues = json.loads(secret.read())
-        
+
     @staticmethod
     def __initDBConnections():
         stockDB = sqlite3.connect(Config.__stockDB)
         backTestDB = sqlite3.connect(Config.__backtestingDB)
         Config.__dbConnections = [stockDB, backTestDB]
-        
+
     @staticmethod
     def getBrokerSession():
         Config.__loadConfigurations()
-        username = Config.__secretValues['Broker']['username']
-        password = Config.__secretValues['Broker']['password']
-        url = Config.__secretValues['Broker']['url']
-        
+        broker = list(Config.__secretValues.keys())[0]
+        username = Config.__secretValues[broker]["username"]
+        password = Config.__secretValues[broker]["password"]
+        url = Config.__secretValues[broker]["url"]
+
         # TODO: Implement Broker Session
         # eg: req = Config.__brokerSession.post(url)
-        
-    
